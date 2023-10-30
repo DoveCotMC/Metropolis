9735521914c6b4ba8eac660bd4cfde1dec0d5b6a
Arrokoth <arrokoth233@gmail.com>
2023-10-30 16:38:31 +0100

    能跑

---
+++ src/main/java/team/dovecotmc/metropolis/metropolis/Metropolis.java
@@ -0,0 +1,15 @@
+package team.dovecotmc.metropolis.metropolis;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author Arrokoth
+ * @project Metropolis
+ * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
+ */
+public class Metropolis implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/team/dovecotmc/metropolis/metropolis/client/MetropolisClient.java
@@ -0,0 +1,15 @@
+package team.dovecotmc.metropolis.metropolis.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author Arrokoth
+ * @project Metropolis
+ * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
+ */
+public class MetropolisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
