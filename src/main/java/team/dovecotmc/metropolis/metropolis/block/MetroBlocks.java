931da3b6534567367142c935cb7e3def0dd10871
Arrokoth <arrokoth233@gmail.com>
2023-11-04 23:41:16 +0100

    简单改一下，方便之后禁用

--- src/main/java/team/dovecotmc/metropolis/metropolis/block/MetroBlocks.java
+++ src/main/java/team/dovecotmc/metropolis/metropolis/block/MetroBlocks.java
@@ -15,25 +15,29 @@ import team.dovecotmc.metropolis.metropolis.Metropolis;
  * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
  */
 public class MetroBlocks {
-    public static final Block BLOCK_CABLE_A = register("cable_a", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_1 = register("cable_a_1", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_2 = register("cable_a_2", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_3 = register("cable_a_3", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_4 = register("cable_a_4", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_5 = register("cable_a_5", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_6 = register("cable_a_6", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_7 = register("cable_a_7", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_8 = register("cable_a_8", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
-    public static final Block BLOCK_CABLE_A_9 = register("cable_a_9", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A = registerDovecotFeatures("cable_a", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_1 = registerDovecotFeatures("cable_a_1", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_2 = registerDovecotFeatures("cable_a_2", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_3 = registerDovecotFeatures("cable_a_3", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_4 = registerDovecotFeatures("cable_a_4", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_5 = registerDovecotFeatures("cable_a_5", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_6 = registerDovecotFeatures("cable_a_6", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_7 = registerDovecotFeatures("cable_a_7", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_8 = registerDovecotFeatures("cable_a_8", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
+    public static final Block BLOCK_CABLE_A_9 = registerDovecotFeatures("cable_a_9", new BlockCable(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).noCollision().nonOpaque()));
     // 隧道灯
-    public static final Block BLOCK_TUNNEL_LIGHT_A = register("tunnel_light_a", new BlockTunnelLight(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).nonOpaque()));
-    public static final Block BLOCK_TICKET_MACHINE_0 = register("ticket_machine_0", new BlockSmallerTickerMachine(FabricBlockSettings.of(Material.METAL, MapColor.DIAMOND_BLUE).nonOpaque()));
-    public static final Block BLOCK_TICKET_MACHINE_1 = register("ticket_machine_1", new BlockTicketMachine(FabricBlockSettings.of(Material.METAL, MapColor.DIAMOND_BLUE).nonOpaque()));
+    public static final Block BLOCK_TUNNEL_LIGHT_A = registerDovecotFeatures("tunnel_light_a", new BlockTunnelLight(FabricBlockSettings.of(Material.METAL, MapColor.STONE_GRAY).nonOpaque()));
+    public static final Block BLOCK_TICKET_MACHINE_A = registerDovecotFeatures("ticket_machine_a", new BlockSmallerTickerMachine(FabricBlockSettings.of(Material.METAL, MapColor.DIAMOND_BLUE).nonOpaque()));
+    public static final Block BLOCK_TICKET_MACHINE_B = registerDovecotFeatures("ticket_machine_b", new BlockTicketMachine(FabricBlockSettings.of(Material.METAL, MapColor.DIAMOND_BLUE).nonOpaque()));
     // 天花板
-    public static final Block BLOCK_TICKET_BARRIER_ENTRANCE_0 = register("ticket_barrier_entrance_0", new BlockBeyondTicketBarrier(true));
-    public static final Block BLOCK_TICKET_BARRIER_EXIT_0 = register("ticket_barrier_exit_0", new BlockBeyondTicketBarrier(false));
-    public static final Block BLOCK_CEILING_A = register("ceiling_a", new BlockCeilingA(FabricBlockSettings.of(Material.METAL, MapColor.GRAY).nonOpaque().luminance(1)));
-    public static final Block BLOCK_CEILING_A_LIGHT = register("ceiling_a_light", new BlockCeilingA(FabricBlockSettings.of(Material.METAL, MapColor.GRAY).nonOpaque().luminance(15)));
+    public static final Block BLOCK_TICKET_BARRIER_ENTRANCE_A = registerDovecotFeatures("ticket_barrier_entrance_a", new BlockBeyondTicketBarrier(true));
+    public static final Block BLOCK_TICKET_BARRIER_EXIT_A = registerDovecotFeatures("ticket_barrier_exit_a", new BlockBeyondTicketBarrier(false));
+    public static final Block BLOCK_CEILING_A = registerDovecotFeatures("ceiling_a", new BlockCeilingA(FabricBlockSettings.of(Material.METAL, MapColor.GRAY).nonOpaque().luminance(1)));
+    public static final Block BLOCK_CEILING_A_LIGHT = registerDovecotFeatures("ceiling_a_light", new BlockCeilingA(FabricBlockSettings.of(Material.METAL, MapColor.GRAY).nonOpaque().luminance(15)));
+
+    private static Block registerDovecotFeatures(String id, Block block) {
+        return Registry.register(Registry.BLOCK, new Identifier(Metropolis.MOD_ID, id), block);
+    }
 
     private static Block register(String id, Block block) {
         return Registry.register(Registry.BLOCK, new Identifier(Metropolis.MOD_ID, id), block);
--- src/main/java/team/dovecotmc/metropolis/metropolis/item/MetroItems.java
+++ src/main/java/team/dovecotmc/metropolis/metropolis/item/MetroItems.java
@@ -14,27 +14,32 @@ import team.dovecotmc.metropolis.metropolis.block.MetroBlocks;
  * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
  */
 public class MetroItems {
-    public static final Item ITEM_CABLE_A = register("cable_a", new BlockItem(MetroBlocks.BLOCK_CABLE_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_1 = register("cable_a_1", new BlockItem(MetroBlocks.BLOCK_CABLE_A_1, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_2 = register("cable_a_2", new BlockItem(MetroBlocks.BLOCK_CABLE_A_2, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_3 = register("cable_a_3", new BlockItem(MetroBlocks.BLOCK_CABLE_A_3, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_4 = register("cable_a_4", new BlockItem(MetroBlocks.BLOCK_CABLE_A_4, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_5 = register("cable_a_5", new BlockItem(MetroBlocks.BLOCK_CABLE_A_5, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_6 = register("cable_a_6", new BlockItem(MetroBlocks.BLOCK_CABLE_A_6, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_7 = register("cable_a_7", new BlockItem(MetroBlocks.BLOCK_CABLE_A_7, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_8 = register("cable_a_8", new BlockItem(MetroBlocks.BLOCK_CABLE_A_8, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CABLE_A_9 = register("cable_a_9", new BlockItem(MetroBlocks.BLOCK_CABLE_A_9, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    // 已在其它模组实现的功能
+    public static final Item ITEM_CABLE_A = registerDovecotFeature("cable_a", new BlockItem(MetroBlocks.BLOCK_CABLE_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_1 = registerDovecotFeature("cable_a_1", new BlockItem(MetroBlocks.BLOCK_CABLE_A_1, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_2 = registerDovecotFeature("cable_a_2", new BlockItem(MetroBlocks.BLOCK_CABLE_A_2, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_3 = registerDovecotFeature("cable_a_3", new BlockItem(MetroBlocks.BLOCK_CABLE_A_3, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_4 = registerDovecotFeature("cable_a_4", new BlockItem(MetroBlocks.BLOCK_CABLE_A_4, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_5 = registerDovecotFeature("cable_a_5", new BlockItem(MetroBlocks.BLOCK_CABLE_A_5, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_6 = registerDovecotFeature("cable_a_6", new BlockItem(MetroBlocks.BLOCK_CABLE_A_6, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_7 = registerDovecotFeature("cable_a_7", new BlockItem(MetroBlocks.BLOCK_CABLE_A_7, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_8 = registerDovecotFeature("cable_a_8", new BlockItem(MetroBlocks.BLOCK_CABLE_A_8, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CABLE_A_9 = registerDovecotFeature("cable_a_9", new BlockItem(MetroBlocks.BLOCK_CABLE_A_9, new Item.Settings().group(Metropolis.ITEM_GROUP)));
     // TODO: 隧道灯
-    public static final Item ITEM_TUNNEL_LIGHT_A = register("tunnel_light_a", new BlockItem(MetroBlocks.BLOCK_TUNNEL_LIGHT_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_TICKET_MACHINE_0 = register("ticket_machine_0", new BlockItem(MetroBlocks.BLOCK_TICKET_MACHINE_0, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_TICKET_MACHINE_1 = register("ticket_machine_1", new BlockItem(MetroBlocks.BLOCK_TICKET_MACHINE_1, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_TUNNEL_LIGHT_A = registerDovecotFeature("tunnel_light_a", new BlockItem(MetroBlocks.BLOCK_TUNNEL_LIGHT_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_TICKET_MACHINE_A = registerDovecotFeature("ticket_machine_a", new BlockItem(MetroBlocks.BLOCK_TICKET_MACHINE_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_TICKET_MACHINE_B = registerDovecotFeature("ticket_machine_b", new BlockItem(MetroBlocks.BLOCK_TICKET_MACHINE_B, new Item.Settings().group(Metropolis.ITEM_GROUP)));
     // byd老闸机
-    public static final Item ITEM_TICKET_BARRIER_ENTRANCE_0 = register("ticket_barrier_entrance_0", new BlockItem(MetroBlocks.BLOCK_TICKET_BARRIER_ENTRANCE_0, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_TICKET_BARRIER_EXIT_0 = register("ticket_barrier_exit_0", new BlockItem(MetroBlocks.BLOCK_TICKET_BARRIER_EXIT_0, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_TICKET_BARRIER_ENTRANCE_A = registerDovecotFeature("ticket_barrier_entrance_a", new BlockItem(MetroBlocks.BLOCK_TICKET_BARRIER_ENTRANCE_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_TICKET_BARRIER_EXIT_A = registerDovecotFeature("ticket_barrier_exit_a", new BlockItem(MetroBlocks.BLOCK_TICKET_BARRIER_EXIT_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
     // 国铁闸机
     // 天花板
-    public static final Item ITEM_CEILING_A = register("ceiling_a", new BlockItem(MetroBlocks.BLOCK_CEILING_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
-    public static final Item ITEM_CEILING_A_LIGHT = register("ceiling_a_light", new BlockItem(MetroBlocks.BLOCK_CEILING_A_LIGHT, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+    public static final Item ITEM_CEILING_A = registerDovecotFeature("ceiling_a", new BlockItem(MetroBlocks.BLOCK_CEILING_A, new Item.Settings().group(Metropolis.ITEM_GROUP)));
+//    public static final Item
+
+    public static Item registerDovecotFeature(String id, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Metropolis.MOD_ID, id), item);
+    }
 
     public static Item register(String id, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Metropolis.MOD_ID, id), item);
