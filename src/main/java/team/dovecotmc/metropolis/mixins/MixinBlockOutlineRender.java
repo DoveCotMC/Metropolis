a2374e6fbfa4144ceda2b763ff85798161c00b17
Arrokoth <arrokoth233@gmail.com>
2024-07-01 11:28:32 +0200

    Bridge creator

--- src/main/java/team/dovecotmc/metropolis/mixins/MixinBlockOutlineRender.java
+++ src/main/java/team/dovecotmc/metropolis/mixins/MixinBlockOutlineRender.java
@@ -11,7 +11,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -38,7 +40,29 @@ public abstract class MixinBlockOutlineRender {
         if (MetropolisClient.BLOCK_PLACE_HUD.shouldRender && world != null) {
             Station station = MtrStationUtil.getStationByPos(pos, world);
             if (station != null) {
-                drawCuboidShapeOutline(matrices, vertexConsumer, state.getOutlineShape(world, pos, ShapeContext.of(entity)), (double)pos.getX() - cameraX, (double)pos.getY() - cameraY, (double)pos.getZ() - cameraZ, ColorHelper.Argb.getRed(station.color) / 255f, ColorHelper.Argb.getGreen(station.color) / 255f, ColorHelper.Argb.getBlue(station.color) / 255f, 0.4F);
+                float red = ColorHelper.Argb.getRed(station.color) / 255f;
+                float green = ColorHelper.Argb.getGreen(station.color) / 255f;
+                float blue = ColorHelper.Argb.getBlue(station.color) / 255f;
+                drawCuboidShapeOutline(matrices, vertexConsumer, state.getOutlineShape(world, pos, ShapeContext.of(entity)), (double)pos.getX() - cameraX, (double)pos.getY() - cameraY, (double)pos.getZ() - cameraZ, red, green, blue, 0.4F);
+
+//                double offsetX = (double)pos.getX() - cameraX;
+//                double offsetY = (double)pos.getY() - cameraY;
+//                double offsetZ = (double)pos.getZ() - cameraZ;
+//                MatrixStack.Entry entry = matrices.peek();
+//                VoxelShape shape = VoxelShapes.fullCube();
+//                shape.forEachEdge((minX, minY, minZ, maxX, maxY, maxZ) -> {
+//                    float k = (float)(maxX - minX);
+//                    float l = (float)(maxY - minY);
+//                    float m = (float)(maxZ - minZ);
+//                    float n = MathHelper.sqrt(k * k + l * l + m * m);
+//                    k /= n;
+//                    l /= n;
+//                    m /= n;
+//                    RenderSystem.setShader(GameRenderer::getPositionColorShader);
+//                    // .normal(entry.getNormalMatrix(), k, l, m)
+//                    vertexConsumer.vertex(entry.getPositionMatrix(), (float)(minX + offsetX), (float)(minY + offsetY), (float)(minZ + offsetZ)).color(red, green, blue, 0.2f).next();
+//                    vertexConsumer.vertex(entry.getPositionMatrix(), (float)(maxX + offsetX), (float)(maxY + offsetY), (float)(maxZ + offsetZ)).color(red, green, blue, 0.2f).next();
+//                });
             }
         }
     }
