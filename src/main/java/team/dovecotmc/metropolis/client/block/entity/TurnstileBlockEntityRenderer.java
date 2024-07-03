7477cf468291ecc5f221492750529e5de1b7a3f8
Arrokoth <arrokoth233@gmail.com>
2024-07-03 23:13:24 +0200

    glowwww

--- src/main/java/team/dovecotmc/metropolis/client/block/entity/TurnstileBlockEntityRenderer.java
+++ src/main/java/team/dovecotmc/metropolis/client/block/entity/TurnstileBlockEntityRenderer.java
@@ -1,17 +1,21 @@
 package team.dovecotmc.metropolis.client.block.entity;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.*;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import team.dovecotmc.metropolis.Metropolis;
 import team.dovecotmc.metropolis.block.BlockTurnstile;
 import team.dovecotmc.metropolis.block.entity.BlockEntityTurnstile;
 
@@ -58,11 +62,62 @@ public class TurnstileBlockEntityRenderer implements BlockEntityRenderer<BlockEn
                 matrices.pop();
             }
 
+
+
             matrices.scale(1f / 16f, 1f / 16f, 1f / 16f);
             matrices.translate(8f, 8f, 8f);
             matrices.multiply(Quaternion.fromEulerXyzDegrees(new Vec3f(0, -facing.asRotation() - 180, 0)));
             matrices.translate(-8f, -8f, -8f);
             matrices.scale(16f, 16f, 16f);
+
+            RenderSystem.enableBlend();
+            RenderSystem.enableDepthTest();
+            RenderSystem.defaultBlendFunc();
+
+            float lightFactor = Math.min(Math.max((Math.max(LightmapTextureManager.getSkyLightCoordinates(light), LightmapTextureManager.getBlockLightCoordinates(light))) / 15f, 7f / 15f), 13f / 15f);
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder builder = tessellator.getBuffer();
+            float d = 4f / 16f;
+
+            // Forwards
+            matrices.push();
+            matrices.translate(0, 0, -d - 0.1f / 16f);
+
+            RenderSystem.setShaderTexture(0, new Identifier(Metropolis.MOD_ID, "textures/block/turnstile_light_entry.png"));
+            builder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+            builder.vertex(matrices.peek().getPositionMatrix(), 0, 1, 0).texture(1, 0).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 1, 1, 0).texture(0, 0).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 1, 0, 0).texture(0, 1).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 0, 0, 0).texture(1, 1).next();
+
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderColor(lightFactor, lightFactor, lightFactor, 1f);
+
+            tessellator.draw();
+            matrices.pop();
+
+            // Backwards
+            matrices.push();
+            matrices.translate(0.5f, 0.5f, 0.5f);
+            matrices.multiply(Quaternion.fromEulerXyzDegrees(new Vec3f(0, 180, 0)));
+            matrices.translate(-0.5f, -0.5f, -0.5f);
+            matrices.translate(0, 0, -d - 0.1f / 16f);
+
+            RenderSystem.setShaderTexture(0, new Identifier(Metropolis.MOD_ID, "textures/block/turnstile_light_exit.png"));
+            builder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+            builder.vertex(matrices.peek().getPositionMatrix(), 0, 1, 0).texture(1, 0).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 1, 1, 0).texture(0, 0).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 1, 0, 0).texture(0, 1).next();
+            builder.vertex(matrices.peek().getPositionMatrix(), 0, 0, 0).texture(1, 1).next();
+
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderColor(lightFactor, lightFactor, lightFactor, 1f);
+
+            tessellator.draw();
+            matrices.pop();
+
+            RenderSystem.disableBlend();
+            RenderSystem.disableDepthTest();
 
             BlockEntityTurnstile.EnumTurnstileType type = BlockEntityTurnstile.EnumTurnstileType.get(block.get(BlockTurnstile.TYPE));
 //            BlockEntityTurnstile.EnumTurnstileType type = ((BlockTurnstile) block.getBlock()).type;
