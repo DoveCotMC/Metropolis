8694737254e0b3339eb0faf9127c577388c9a765
Arrokoth233 <arrokoth233@gmail.com>
2026-02-28 23:04:41 +0100

    ber how

--- src/main/java/team/dovecotmc/metropolis/util/MtrStationUtil.java
+++ src/main/java/team/dovecotmc/metropolis/util/MtrStationUtil.java
@@ -16,18 +16,18 @@ import java.util.Set;
  * @copyright Copyright © 2024 Arrokoth All Rights Reserved.
  */
 public class MtrStationUtil {
-//    public static Set<Station> getStations(Level world) {
-//        if (world.isClientSide()) {
-//            return ClientData.STATIONS;
-//        }
-//        return RailwayData.getInstance(world).stations;
-//    }
-//
-//    public static org.mtr.core.map.Station getStationByPos(BlockPos pos, Level world) {
-//        if (world.isClientSide()) {
-//            Simulator.
-//            return RailwayData.getStation(getStations(world), ClientData.DATA_CACHE, pos);
-//        }
-//        return RailwayData.getStation(getStations(world), RailwayData.getInstance(world).dataCache, pos);
-//    }
+    public static Set<Station> getStations(Level world) {
+        if (world.isClientSide()) {
+            return ClientData.STATIONS;
+        }
+        return RailwayData.getInstance(world).stations;
+    }
+
+    public static org.mtr.core.map.Station getStationByPos(BlockPos pos, Level world) {
+        if (world.isClientSide()) {
+            Simulator.
+            return RailwayData.getStation(getStations(world), ClientData.DATA_CACHE, pos);
+        }
+        return RailwayData.getStation(getStations(world), RailwayData.getInstance(world).dataCache, pos);
+    }
 }
