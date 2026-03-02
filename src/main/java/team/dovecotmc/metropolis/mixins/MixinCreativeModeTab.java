e3ee71fc9df92e9e99bac703dc229b254f17abb2
Arrokoth233 <arrokoth233@gmail.com>
2026-03-02 18:40:34 +0100

    ok

--- src/main/java/team/dovecotmc/metropolis/mixins/MixinCreativeModeTab.java
+++ src/main/java/team/dovecotmc/metropolis/mixins/MixinCreativeModeTab.java
@@ -1,4 +1,44 @@
 package team.dovecotmc.metropolis.mixins;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import team.dovecotmc.metropolis.Metropolis;
+
+@Mixin(CreativeModeInventoryScreen.class)
 public class MixinCreativeModeTab {
+    @Shadow
+    private static CreativeModeTab selectedTab;
+
+    @Inject(
+            method = "render",
+            at = @At("TAIL")
+    )
+    private void met$localizationHint(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) {
+        if (!selectedTab.equals(Metropolis.ITEM_GROUP))
+            return;
+
+        Component component = Component.translatable("help.metropolis.localization");
+        if (component.getString().isBlank()) {
+            return;
+        }
+
+        int w = guiGraphics.guiWidth();
+        int h = guiGraphics.guiHeight();
+
+        guiGraphics.drawString(
+                Minecraft.getInstance().font,
+                component,
+                w / 2 - Minecraft.getInstance().font.width(component) / 2,
+                (h / 2 - Minecraft.getInstance().font.lineHeight / 2 - 100) / 2,
+                0xFFFFFF
+        );
+    }
 }
